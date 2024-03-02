f7bcc88f43daecf8b20a42a6f53cf3e37f1bce32
nisarga636 <=>
2024-03-02 20:42:42 +0530

    signup

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup= async(req, res) => {
+    const{ username,email,password} =req.body;
+
+    if (!username || !email || !password || username==='' || email==='' || password===''){
+    return res.status(400).json({message: "All fields are reuired"});}
+
+    const hashedPassword = bcrypt.hashSync(password,10);
+
+    const newUser= new User({
+        username,
+        email,
+        password:hashedPassword,
+    });
+
+    try {
+        await newUser.save();
+    res.json({message: 'signup successful'});        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+    
+};
